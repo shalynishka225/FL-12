d87ef7b6e6c274d0002e9dfc91b5ec0f03f28bb9
shalynishka225 <shalynishka225@gmail.com>
2020-02-11 20:20:05 +0200

    new version

--- FL12_HW12/homework/src/app.js
+++ FL12_HW12/homework/src/app.js
@@ -1,120 +1,83 @@
-const root = document.getElementById('root');
-let objArray = [];
-let id = 1;
-
-const createForm = (id, word, definition,chbox) => {
-
-  const UIContainer = document.createElement('div');
-  const formHeader = document.createElement('div');
-  const formHeaderItems = document.createElement('div');
-  const formCounter = document.createElement('div');
-  const counter = document.createElement('span');
-  const removeButton = document.createElement('div');
-  const removeButtonA = document.createElement('a');
-  const changeButton = document.createElement('div');
-  const changeButtonA = document.createElement('a');
-  const formCheckBox = document.createElement('div');
-  const checkBox = document.createElement('input');
-  const formContext = document.createElement('div');
-  const formContextItems = document.createElement('div');
-  const wordField = document.createElement('div');
-  const wordInput = document.createElement('input');
-  const wordLabel = document.createElement('label');
-  const definitionField = document.createElement('div');
-  const definitionInput = document.createElement('input');
-  const definitionLabel = document.createElement('label');
-
-  UIContainer.classList.toggle('UIContainer');
-  formHeader.classList.toggle('formHeader');
-  formHeaderItems.classList.toggle('formHeaderItems');
-  formCounter.classList.toggle('formCounter');
-  counter.classList.toggle('counter');
-  removeButton.classList.toggle('removeButton');
-  removeButtonA.classList.toggle('myButton');
-  changeButton.classList.toggle('changeButton');
-  changeButtonA.classList.toggle('myButton');
-  formCheckBox.classList.toggle('formCheckBox');
-  checkBox.classList.toggle('checkbox');
-  formContext.classList.toggle('formContext');
-  formContextItems.classList.toggle('formContextItems');
-  wordField.classList.toggle('wordField');
-  wordInput.classList.toggle('wordInput');
-  wordLabel.classList.toggle('wordLabel');
-  definitionField.classList.toggle('definitionField');
-  definitionInput.classList.toggle('definitionInput');
-  definitionLabel.classList.toggle('definitionLabel');
-
-  root.appendChild(UIContainer);
-  UIContainer.appendChild(formHeader);
-  formHeader.appendChild(formHeaderItems);
-  formHeaderItems.appendChild(formCounter);
-  formCounter.appendChild(counter);
-  formHeaderItems.appendChild(removeButton);
-  removeButton.appendChild(removeButtonA)
-  formHeaderItems.appendChild(changeButton);
-  changeButton.appendChild(changeButtonA);
-  formHeaderItems.appendChild(formCheckBox);
-  formCheckBox.appendChild(checkBox);
-
-  UIContainer.appendChild(formContext);
-  formContext.appendChild(formContextItems);
-  formContextItems.appendChild(wordField);
-  wordField.appendChild(wordInput);
-  wordField.appendChild(wordLabel);
-  formContextItems.appendChild(definitionField);
-  definitionField.appendChild(definitionInput);
-  definitionField.appendChild(definitionLabel);
-
-  counter.innerText = id;
-  removeButtonA.innerText = 'Удалить';
-  changeButtonA.innerText = 'Изменить';
-  checkBox.setAttribute('type','checkbox');
-  wordInput.setAttribute('type','text');
-  wordInput.id = 'wordArea';
-  wordInput.setAttribute('name', 'word');
-  wordLabel.setAttribute('for', 'word');
-  wordLabel.innerText= 'термин';
-  definitionInput.setAttribute('type','text');
-  definitionInput.setAttribute('name', 'definition');
-  definitionInput.id = 'definitionArea';
-  definitionLabel.setAttribute('for', 'definition');
-  definitionLabel.innerText = 'определение';
-  checkBox.checked = chbox;
-  removeButtonA.setAttribute('onclick' , 'removeForm(event)');
-
-  return UIContainer;
-}
-
-const createObj = (id, word, def, chbox) => {
-  return {
-    id, word, def, chbox
+const app = {
+  pages: [],
+  init: function() {
+    app.pages = document.querySelectorAll('.page');
+    
+    document.querySelectorAll('.nav-link').forEach((link)=>{
+      link.addEventListener('click', app.nav);
+    });
+    history.replaceState({},'Home', '#home');
+    window.addEventListener('popstate', app.popin);
+  },
+  nav: function(event) {
+    event.preventDefault();
+    let currentPage = event.target.getAttribute('data-target');
+    document.querySelector('.active').classList.remove('active');
+    document.getElementById(currentPage).classList.add('active');
+    history.pushState({}, currentPage, `#${currentPage}`);
+  },
+  popin: function(event) {
+    let hash = location.hash.replace('#', '');
+    document.querySelector('.active').classList.remove('active');
+    document.getElementById(hash).classList.add('active');
   }
+  
 }
 
-const create = document.getElementById('createButton');
-
-create.addEventListener('click' , () => {
-  //createObj(id,'','',false);
-  //console.log(createObj(id,'','',false));
-  createForm(id,'','',false);
-  id++;
-  readInput;
-  console.log(readInput());
+let saveButton = document.getElementById('save-btn');
+saveButton.addEventListener('click', () => {
+  let word = document.getElementById('word').value;
+  let def = document.getElementById('defenition').value;
+  createTask(word,def);
 });
 
-const removeForm = (event) => {
-  event.target.closest('.UIContainer').remove();
-}
+function createTask(wordInp,defInp) {
+  let mainTask = document.createElement('div');
+  let taskContent = document.createElement('div');
+  let taskBtns = document.createElement('div');
+  let word = document.createElement('span');
+  let defenition = document.createElement('span');
+  let changeButton = document.createElement('input');
+  let deleteButton = document.createElement('input');
 
-const readInput = () => {
-  const allforms = document.querySelectorAll('.UIContainer');
+  word.innerText = wordInp;
+  defenition.innerText = defInp;
+
+  mainTask.classList.add('task');
+  taskContent.classList.add('task-content');
+  taskBtns.classList.add('task-buttons');
+  word.classList.add('word');
+  defenition.classList.add('defenition');
+  changeButton.setAttribute('type','button');
+  changeButton.setAttribute('href','#');
+  changeButton.setAttribute('value','Изменить');
+  changeButton.classList.add('button');
+  changeButton.classList.add('nav-link');
+  deleteButton.setAttribute('type','button');
+  deleteButton.setAttribute('href','#');
+  deleteButton.setAttribute('value','Удалить');
+  deleteButton.classList.add('button');
+  deleteButton.id = 'delete';
+  deleteButton.setAttribute('onclick','remove(event)');
+  changeButton.setAttribute('data-target', 'list');
   
-    return allforms.map(oneForm => ({
-      word: oneForm.querySelector('.wordInput').value,
-      def: oneForm.querySelector('.definitionInput').value      
-    }))
+  document.getElementById('root').appendChild(mainTask);
+  mainTask.appendChild(taskContent);
+  taskContent.appendChild(word);
+  taskContent.appendChild(defenition);
+  mainTask.appendChild(taskBtns);
+  taskBtns.appendChild(changeButton);
+  taskBtns.appendChild(deleteButton);
+  app.init();
 }
 
+const remove = (event) => {
+  event.target.closest('.task').remove()
+}
 
+function Task(word,def) {
+  this.word = word;
+  this.def = def;
+}
 
-
+document.addEventListener('DOMContentLoaded', app.init);
